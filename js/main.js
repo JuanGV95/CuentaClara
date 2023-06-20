6457b29e7a417b4eb7ecc68d288ce4da05a16736
Juan David Gonzalez <juandagv95@gmail.com>
2023-06-19 20:25:18 -0500

    correccion de ortografia organizacion del archivo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 //este es un ejemplo que quiero desarrollar para mi proyecto final
 //la idea es un aplicativo con el que se pueda llevar registro de ingresos
 //egresos y mas adelante planeo añadir funciones para control de gastos
-//impuestos y cuotas faltantes para 
 //let cuenta es el valor general de la cuenta
-let balance = 5000;
+let balance = 0;
 
 /*constructores*/
 class Cuentas {
-    constructor(id, cuenta, nombreCuenta, cuotas, valor, valorCuota) {
+    constructor(id, cuenta, nombreCuenta, cuotas, valor) {
         this.id = id,
             this.cuenta = cuenta,
             this.nombreCuenta = nombreCuenta,
@@ -23,7 +22,7 @@ class Ingresos {
     }
 }
 /*constructores*/
-//con esta funcion inicializamos el proceso, se debe ingresar un valor para poder acceder al menu
+//con esta funcion inicializamos el proceso, se debe ingresar un nombre para poder acceder al menu
 function nombre() {
     let nombreI = prompt("Ingresa tu nombre");
     if (nombreI == "") {
@@ -40,13 +39,13 @@ nombre();
 const listaCuentas = [];
 const listaIngresos = [];
 /*Arrays*/
-/* ejemplos de objetos ya creado para usar con filtros/find y sort | en la version final esto se ELIMINA!! */
+/* ejemplos de objetos ya creados para usar con filtros/find y sort, en la version final esto se ELIMINA!! */
 const nuevaCuenta1 = new Cuentas(1, "Ahorro", "Viaje", 12, 2000);
 const nuevaCuenta2 = new Cuentas(2, "Credito", "Credito Estudiantil", 24, 5000);
 const nuevaCuenta3 = new Cuentas(3, "Otro", "Prestamo", 12, 1200);
 const nuevaCuenta4 = new Cuentas(4, "Ahorro", "Moto", 36, 3000);
 listaCuentas.push(nuevaCuenta1, nuevaCuenta2, nuevaCuenta3, nuevaCuenta4);
-/* ejemplos de objetos ya creado para usar con filtros/find y sort */
+/* ejemplos de objetos ya creados para usar con filtros/find y sort */
 /* ejemplos para los ingresos en la version final esto tambien se ELIMINA*/
 const nuevoIngreso1 = new Ingresos(1, "Trabajo freelance", 100);
 const nuevoIngreso2 = new Ingresos(2, "Venta de servicios", 200);
@@ -54,12 +53,83 @@ const nuevoIngreso3 = new Ingresos(3, "Venta portatil", 400);
 listaIngresos.push(nuevoIngreso1, nuevoIngreso2, nuevoIngreso3);
 /* ejemplos para los ingresos */
 
+/* OPERACIONES */
+
+/* OPERACIONES */
+//El Menu fue realizado en base al ejemplo visto en clase 
+function menu() {
+    let salida = false;
+    do {
+        let opciones = parseInt(prompt(`Escoge la opción que deseas realizar
+            1 - Revisar saldo actual
+            2 - Actualizar sueldo fijo
+            3 - Registrar ingresos ocacionales
+            4 - Agregar una cuenta
+            5 - Ver cuentas 
+            6 - Ver ingresos ocacionales
+            0 - Salir`))
+        switch (opciones) {
+            case 1:
+                revSaldo();
+                break
+
+            case 2:
+                baseMensual();
+                break
+            case 3:
+                ingreso();
+                break
+            case 4:
+                tipoDeCuenta();
+                break
+            case 5:
+                verCuentas(listaCuentas);
+                break
+            case 6:
+                verIngresos(listaIngresos);
+                break
+            case 0:
+                alert("Usaste la salida segura, ¡Hasta pronto!");
+                salida = true;
+                break
+            default:
+                alert("Este menú esta en construcción, por favor elige un número habilitado");
+                break
+        }
+    } while (!salida);
+}
+menu();
+
+//Esta funcion nos dice cual es el valor actual en la cuenta que por defecto es 0 cero
+function revSaldo() {
+    alert(`Su saldo actual es de ${balance} USD`);
+}
+//Con esta funcion podemos consignar un valor a nuesta cuenta inicial y lo actualiza con su nuevo valor
+function baseMensual() {
+    let valorIng = parseInt(prompt("Ingresa tu sueldo mensual"));
+    balance = valorIng + balance;
+    alert(`Su nuevo saldo es de $${balance} USD`);
+}
+
+function ingreso() {
+    let nombraIngreso = prompt("¿De dónde viene tu ingreso ocacional?")
+    let valorDelIngreso = parseInt(prompt("¿Cuál es el valor del ingreso?"));
+
+    const nuevoIngreso = new Ingresos(listaIngresos.length + 1, nombraIngreso, valorDelIngreso)
+    balance = valorDelIngreso + balance;
+
+    alert(`Tu saldo actual se ha actualizado.
+tu nuevo saldo es: $${balance}
+    `);
+    listaIngresos.push(nuevoIngreso);
+}
+
 function tipoDeCuenta() {
-    let cuentaAgregada = parseInt(prompt(`Ingresa el tipo de cuenta que deseas agregar
+    let cuentaAgregada = parseInt(prompt(`Escoge el tipo de cuenta que deseas agregar
     1 - Ahorro
-    2 - Creditos
+    2 - Crédito
     3 - Otro
-    4 - Volver atras
+    4 - Volver atrás
     `))
     switch (cuentaAgregada) {
         case 1:
@@ -82,18 +152,18 @@ function tipoDeCuenta() {
     function ahorros() {
         let cuentaTipo = "Ahorro";
         let nombrarCuenta = prompt("Nombra tu ahorro");
-        let cuotasPago = parseInt(prompt("¿En cuantas cuotas quieres lograr tu meta?"))
-        let valorCuenta = parseInt(prompt("¿Cuanto quieres ahorrar?"));
+        let cuotasPago = parseInt(prompt("¿En cuántas cuotas quieres lograr tu meta?"))
+        let valorCuenta = parseInt(prompt("¿Cuánto quieres ahorrar?"));
 
         const nuevaCuenta = new Cuentas(listaCuentas.length + 1, cuentaTipo, nombrarCuenta, cuotasPago, valorCuenta);
         //pusheamos al array:
         listaCuentas.push(nuevaCuenta);
     }
     function credito() {
-        let cuentaTipo = "Credito";
+        let cuentaTipo = "Crédito";
         let nombrarCuenta = prompt("Nombre de tu credito");
-        let cuotasPago = parseInt(prompt("Elige las cuotas de tu credito"))
-        let valorCuenta = parseInt(prompt("Cual es el valor de tu credito?"));
+        let cuotasPago = parseInt(prompt("Elige las cuotas de tu crédito"))
+        let valorCuenta = parseInt(prompt("¿Cuál es el valor de tu crédito?"));
 
         const nuevaCuenta = new Cuentas(listaCuentas.length + 1, cuentaTipo, nombrarCuenta, cuotasPago, valorCuenta)
         //pusheamos al array:
@@ -101,9 +171,9 @@ function tipoDeCuenta() {
     }
     function otrasCuentas() {
         let cuentaTipo = "Otro";
-        let nombrarCuenta = prompt("Nombra tu Gasto");
+        let nombrarCuenta = prompt("Nombra tu gasto");
         let cuotasPago = parseInt(prompt("Elige las cuotas de tu gasto"))
-        let valorCuenta = parseInt(prompt("Cual es el valor del gasto?"));
+        let valorCuenta = parseInt(prompt("¿Cuál es el valor del gasto?"));
 
         const nuevaCuenta = new Cuentas(listaCuentas.length + 1, cuentaTipo, nombrarCuenta, cuotasPago, valorCuenta)
         //pusheamos al array:
@@ -111,18 +181,17 @@ function tipoDeCuenta() {
     }
 
 }
+
 function verCuentas(array) {
-    //Un ciclo que me permita recorrer todo un array de objetos y acceder a sus propiedades:
     console.log(`Tus cuentas son: `)
-    //FOR OF: sirve para recorrer un array por dentro
     for (let Cuentas of array) {
         console.log(Cuentas.id, Cuentas.cuenta, Cuentas.nombreCuenta, Cuentas.cuotas, Cuentas.valor)
     }
-    let opcionCuentas = parseInt(prompt(`Que deseas hacer?
+    let opcionCuentas = parseInt(prompt(`¿Qué deseas hacer?
     1 - Filtrar cuenta
     2 - Ordenar listado
     3 - Eliminar cuenta
-    4 - Volver atras
+    4 - Volver atrás
     `));
     switch (opcionCuentas) {
         case 1:
@@ -138,11 +207,41 @@ function verCuentas(array) {
             menu();
             break
         default:
-            alert("Elige una opcion del menu");
+            alert("Elige una opción del menú");
             verCuentas();
     }
 }
-/* Filtros */
+
+function verIngresos(array) {
+    console.log(`Tus ingresos fueron: `)
+    for (let Ingresos of array) {
+        console.log(Ingresos.id, Ingresos.nombreIngreso, Ingresos.valorIngreso)
+    }
+    let opcionIngresos = parseInt(prompt(`¿Qué deseas hacer?
+    1 - Filtrar ingreso
+    2 - Ordenar listado
+    3 - Eliminar ingreso
+    4 - Volver atrás
+    `))
+    switch (opcionIngresos) {
+        case 1:
+            buscarIngreso(listaIngresos);
+            break
+        case 2:
+            ordenarIngreso(listaIngresos);
+            break
+        case 3:
+            eliminarIngreso(listaIngresos);
+            break
+        case 4:
+            menu();
+            break
+        default:
+            alert("Elige una opción del menú");
+            verIngresos();
+    }
+}
+/* FILTROS */
 //buscar cuenta
 function buscarCuenta(array) {
     let buscaCuentas = prompt("Elige el nombre de cuenta o tipo de cuenta que quieras filtrar");
@@ -150,7 +249,7 @@ function buscarCuenta(array) {
         (dato) => dato.nombreCuenta.toLowerCase().includes(buscaCuentas.toLowerCase()) || dato.cuenta.toLowerCase().includes(buscaCuentas.toLowerCase())
     )
     if (busqueda.length == 0) {
-        console.log(`La Cuenta ${buscaCuentas} no estra en nuestro listado`)
+        console.log(`La Cuenta ${buscaCuentas} no está en nuestro listado`)
     } else {
         verCuentas(busqueda);
     }
@@ -162,7 +261,7 @@ function buscarIngreso(array) {
         (dato) => dato.nombreIngreso.toLowerCase().includes(buscaIngresos.toLowerCase()) 
     )
     if (busqueda.length == 0) {
-        console.log(`La Cuenta ${buscaIngresos} no estra en nuestro listado`)
+        console.log(`El ingreso ${buscaIngresos} no está en nuestro listado`)
     } else {
         verIngresos(busqueda);
     }
@@ -219,11 +318,9 @@ function ordenarIngresoMenorMayor(array){
        });
        verIngresos(arrayAlfabetico)
    }
-   
-
 //eliminar cuentas
 function eliminar(array){
-    let eliminarID = parseInt(prompt("Elige el Id de la cuenta a eliminar"))
+    let eliminarID = parseInt(prompt("Elige el ID de la cuenta a eliminar"))
     let cuentaID = array.map(cuenta => cuenta.id);
     console.log(cuentaID);
 
@@ -234,7 +331,7 @@ function eliminar(array){
 }
 //eliminar ingreso
 function eliminarIngreso(array){
-    let eliminarID = parseInt(prompt("Elige el Id del ingreso a eliminar"))
+    let eliminarID = parseInt(prompt("Elige el ID del ingreso a eliminar"))
     let ingresoID = array.map(cuenta => cuenta.id);
     console.log(ingresoID);
 
@@ -243,15 +340,12 @@ function eliminarIngreso(array){
     array.splice(indice, 1)
     verIngresos(array);
 }
-
-
-/* Filtros */
 function ordenar(array){
     let opcion = parseInt(prompt(`
     1 - Ordenar de menor a mayor
     2 - Ordenar de mayor a menor
-    3 - Ordenar alfabeticamente
-    4 - Volver atras
+    3 - Ordenar alfabéticamente
+    4 - Volver atrás
     `))
     switch(opcion){
         case 1:
@@ -267,7 +361,7 @@ function ordenar(array){
             menu();
             break 
             default:
-                alert("elige una opcion valida")
+                alert("Elige una opción válida")
                 ordenar();           
     }
 }
@@ -276,8 +370,8 @@ function ordenarIngreso(array){
     let opcion = parseInt(prompt(`
     1 - Ordenar de menor a mayor
     2 - Ordenar de mayor a menor
-    3 - Ordenar alfabeticamente
-    4 - Volver atras
+    3 - Ordenar alfabéticamente
+    4 - Volver atrás
     `))
     switch(opcion){
         case 1:
@@ -293,108 +387,8 @@ function ordenarIngreso(array){
             menu();
             break  
             default:
-                alert("elige una opcion valida");
+                alert("Elige una opción válida");
                 ordenarIngreso();          
     }
 }
-
-function verIngresos(array) {
-    console.log(`Tus ingresos fueron: `)
-    for (let Ingresos of array) {
-        console.log(Ingresos.id, Ingresos.nombreIngreso, Ingresos.valorIngreso)
-    }
-    let opcionIngresos = parseInt(prompt(`Que deseas hacer?
-    1 - Filtrar ingreso
-    2 - Ordenar listado
-    3 - Eliminar ingreso
-    4 - Volver atras
-    `))
-    switch (opcionIngresos) {
-        case 1:
-            buscarIngreso(listaIngresos);
-            break
-        case 2:
-            ordenarIngreso(listaIngresos);
-            break
-        case 3:
-            eliminarIngreso(listaIngresos);
-            break
-        case 4:
-            menu();
-            break
-        default:
-            alert("Elige una opcion del menu");
-            verIngresos();
-    }
-}
-
-
-//Esta funcion nos dice cual es el valor actual en la cuenta que por defecto es 0 cero
-function revSaldo() {
-    alert(`Su saldo actual es de ${balance} USD`);
-}
-//Con esta funcion podemos consignar un valor a nuesta cuenta inicial y lo actualiza con su nuevo valor
-function baseMensual() {
-    let valorIng = parseInt(prompt("Ingresa tu sueldo mensual"));
-    balance = valorIng + balance;
-    alert(`Su nuevo saldo es de $${balance} USD`);
-}
-function ingreso() {
-    let nombraIngreso = prompt("De donde viene tu ingreso")
-    let valorDelIngreso = parseInt(prompt("Cual es el valor del ingreso?"));
-
-    const nuevoIngreso = new Ingresos(listaIngresos.length + 1, nombraIngreso, valorDelIngreso)
-    balance = valorDelIngreso + balance;
-
-    alert(`Tu saldo actual se a actualizado.
-tu nuevo saldo es: $${balance}
-    `);
-    listaIngresos.push(nuevoIngreso);
-}
-/* OPERACIONES */
-
-/* OPERACIONES */
-
-//El Menu fue realizado en base al ejemplo visto en clase 
-function menu() {
-    let salida = false;
-    do {
-        let opciones = parseInt(prompt(`Ingresa la opción que deseas realizar
-            1 - Revisar saldo Actual
-            2 - Ingresar sueldo
-            3 - Ingresos varios
-            4 - Agregar una cuenta
-            5 - Ver cuentas 
-            6 - Ver ingresos ocacionales
-            0 - Salir`))
-        switch (opciones) {
-            case 1:
-                revSaldo();
-                break
-
-            case 2:
-                baseMensual();
-                break
-            case 3:
-                ingreso();
-                break
-            case 4:
-                tipoDeCuenta();
-                break
-            case 5:
-                verCuentas(listaCuentas);
-                break
-            case 6:
-                verIngresos(listaIngresos);
-                break
-            case 0:
-                alert("Usaste la salida segura, ¡hasta pronto!");
-                salida = true;
-                break
-            default:
-                alert("Este menu esta en construcción, por favor elige un número habilitado");
-                break
-        }
-    } while (!salida);
-}
-menu();
+/* FILTROS */
